1722f300e137b96cf85abcfddf6a789e8e09d524
Milan Mazik <mazik1990@hotmail.com>
2023-11-02 18:47:35 +0100

    Szerver API- First route

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+//Útvonalak importálása
+const products = require("./routes/product");
+
+app.use("/api/v1", products);
+
 module.exports = app;
