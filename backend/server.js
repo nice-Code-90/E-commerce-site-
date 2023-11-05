bbe73109cac06d1d05d590937198aab1713e78bc
SzaboPatrik02 <145067605+SzaboPatrik02@users.noreply.github.com>
2023-11-05 15:38:18 +0100

    Connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const app = require("./app");
+const connectDatabase = require('./config/database')
 
 const dotenv = require("dotenv");
 
 // Config fájl
 dotenv.config({ path: "backend/config/config.env" });
+
+//csatlakozas az adatbazishoz
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
   console.log(
