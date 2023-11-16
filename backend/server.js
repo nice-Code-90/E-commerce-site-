9808563df9005d07a09f90fac28ecb3d9d6da833
Milan Mazik <mazik1990@hotmail.com>
2023-11-16 19:04:44 +0100

    Backend - exceptions handle
    Handle uncalled exceptions

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,13 @@ const connectDatabase = require("./config/database");
 
 const dotenv = require("dotenv");
 
+// Handle Uncaught Exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log(`Shutting down server due to uncaught exception`);
+  process.exit(1);
+});
+
 // Config fájl
 dotenv.config({ path: "backend/config/config.env" });
 
