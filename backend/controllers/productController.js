dfd24b341948019c2a619ef34d2c4963210106e8
zolttakacs <145469217+zolttakacs@users.noreply.github.com>
2023-11-16 14:24:50 +0100

    BackEnd --controllers(productController.js) Product Find()

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -12,7 +12,7 @@ exports.newProduct = async (req, res, next) => {
 
 exports.getProducts = async(req, res, next) => {
 
- 
+  const products = await Product.find();
   res.status(200).json({
     success: true,
     count: products.length,
