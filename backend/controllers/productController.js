e7477f00ce7aabd323f641354fcc7929590f3512
zolttakacs <145469217+zolttakacs@users.noreply.github.com>
2023-11-16 14:31:15 +0100

    BackEnd --controllers(productController.js) - if(!product) status

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,4 +23,16 @@ exports.getProducts = async(req, res, next) => {
 exports.getSingleProduct = async (req, res, next) => {
 
   const products = await Product.findById(req.params.id);
+
+  if(!product){
+    return res.status(404).json({
+      success: false,
+      message: 'Product not found'
+    })
+  }
+
+  res.status(200).json({
+    success: true,
+    product
+  })
 }
