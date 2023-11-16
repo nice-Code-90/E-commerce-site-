1cb9f37c1de79b76b098384f7e10562228c679f0
zolttakacs <145469217+zolttakacs@users.noreply.github.com>
2023-11-16 14:16:39 +0100

    BackEnd -- utils(seeder.js) - All Products are added.

--- backend/utils/seeder.js
+++ backend/utils/seeder.js
@@ -12,3 +12,14 @@ const seedProducts = async () => {
     try {
         await Product.deleteMany();
         console.log('Products are deleted.');
+        
+        await Product.insertMany(products)
+        console.log('All Products are added.');
+
+    } catch (error){
+        console.log(error.message);
+        process.exit();
+    }
+}
+
+seedProducts()
