88c905f9e8e2a7cd613a0dc5eddadd7987dd62ba
PetraPongo <pongopetra99@gmail.com>
2023-11-19 03:27:59 +0100

    Backend  - Jelszó reset

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,7 +4,7 @@ const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 const sendToken = require('../utils/jwtToken');
 const sendEmail = require('../utils/sendEmail');
-
+const crypto = require('crypto');
 
 
 //User regisztrálása -> /api/v1/register
@@ -100,6 +100,37 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
 
         return next(new ErrorHandler(error.message, 500))
     }
+
+})
+
+//Reset password => /api/v1/password/reset/:token
+
+exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+
+    //Hash URL token
+
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.token).digest('hex')
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() }
+    })
+
+    if (!user) {
+        return next(new ErrorHandler('Password reset token is invalid or has been expired', 400))
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+        return next(new ErrorHandler('Password does not match', 400))
+    }
+
+    //Új jelszó beállítása
+
+    user.password = req.body.password;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+
+    sendToken(user, 200, res)
 
 })
 
