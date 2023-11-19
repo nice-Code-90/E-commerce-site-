65d706aa8bd21d0212b31be340461fd3273abf37
PetraPongo <pongopetra99@gmail.com>
2023-11-19 01:07:25 +0100

    Backend - Jelszó titkosítása

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,10 @@ app.use(express.json());
 
 //Útvonalak importálása
 const products = require("./routes/product");
+const auth = require("./routes/auth");
 
 app.use("/api/v1", products);
+app.use("/api/v1", auth);
 
 //Middleware to handle errors
 app.use(errorMiddleware);
---
+++ backend/controllers/authController.js
@@ -0,0 +1,24 @@
+const User = require('../models/user');
+const ErrorHandler = require('../utils/errorHandler');
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+
+//User regisztrálása -> /api/v1/register
+
+exports.registerUser = catchAsyncError(async (req, res, next) => {
+
+    const { name, email, password } = req.body;
+    const user = await User.create({
+        name,
+        email,
+        password,
+        avatar: {
+            public_id: 'avatars/kccvibpsuiusmwfepb3m.png',
+            url: 'https://res.cloudinary.com/shopit/image/upload/v1606305757/avatars/kccvibpsuiusmwfepb3m.png'
+        }
+    })
+
+    res.status(201).json({
+        success: true,
+        user
+    })
+})
