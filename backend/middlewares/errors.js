153aed2d4827dd0ca8580a63214f0c17153ea793
SzaboPatrik02 <145067605+SzaboPatrik02@users.noreply.github.com>
2023-11-20 21:01:44 +0100

    Backend -- Hibák kezelése

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -29,6 +29,24 @@ module.exports = (err, req, res, next) => {
       error = new ErrorHandler(message, 400);
     }
 
+    //Handling Mongoose Duplicate Key Errors
+    if(err.code ===11000){
+      const message = `Duplicate ${Object.keys(err.keyValue)} entered`
+      error = new ErrorHandler(message, 400);
+    }
+
+    //Hangling Wrong JWT error
+    if (err.name === 'JsonWebTokenError') {
+      const message = 'JSON Web Token is invalid. Try again!'
+      error = new ErrorHandler(message, 400);
+    }
+
+    //Hangling Expired JWT error
+    if (err.name === 'TokenExpiredError') {
+      const message = 'JSON Web Token is expired. Try again!'
+      error = new ErrorHandler(message, 400);
+    }
+
     res.status(err.statusCode).json({
       success: false,
       message: error.message || "Internal Server Error",
