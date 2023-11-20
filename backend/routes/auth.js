5991737ffd46e0d7f65788fa37b814c6bca064a9
SzaboPatrik02 <145067605+SzaboPatrik02@users.noreply.github.com>
2023-11-20 21:16:30 +0100

    Backend -- Felhasználó kezelés

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser, loginUser, logout, forgotPassword, resetPassword } = require('../controllers/authController');
+const {
+    registerUser,
+    loginUser,
+    logout,
+    forgotPassword,
+    getUserProfile,
+    resetPassword
+} = require('../controllers/authController');
+
+const { isAuthenticatedUser } = require('../middlewares/autH')
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/logout').get(logout);
 router.route('/password/forgot').post(forgotPassword);
 router.route('/password/reset/:token').put(resetPassword);
+
+router.route('/me').get(isAuthenticatedUser, getUserProfile)
 
 
 
