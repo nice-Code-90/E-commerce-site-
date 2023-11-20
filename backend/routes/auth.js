f6600036b8156ee6552b524329fe18270970a415
SzaboPatrik02 <145067605+SzaboPatrik02@users.noreply.github.com>
2023-11-20 22:04:12 +0100

    Backend -- Admin felület

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,10 +9,12 @@ const {
     getUserProfile,
     updatePassword,
     updateProfile,
-    resetPassword
+    resetPassword,
+    allUsers,
+    getUserDetails
 } = require('../controllers/authController');
 
-const { isAuthenticatedUser } = require('../middlewares/autH')
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/autH')
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
@@ -23,6 +25,9 @@ router.route('/password/reset/:token').put(resetPassword);
 router.route('/me').get(isAuthenticatedUser, getUserProfile)
 router.route('/password/update').put(isAuthenticatedUser, updatePassword)
 router.route('/me/update').put(isAuthenticatedUser, updateProfile)
+
+router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers)
+router.route('/admin/user/:id').get(isAuthenticatedUser, authorizeRoles('admin'), getUserDetails)
 
 
 
