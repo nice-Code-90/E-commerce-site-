dcee489763e6e750e0a548c78c2352cae1f79dda
zolttakacs <zoli200200@gmail.com>
2023-11-23 12:45:21 +0100

    BackEnd - controllers(productController.js) Delete Product Review

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -113,3 +113,29 @@ exports.createProductReview = catchAsyncErrors(async(req,res,next) =>{
     success: true
 
 });
+
+// Delete Product Reviews => /api/v1/reviews
+
+exports.deleteReview = catchAsyncErrors(async(req,res,next) => {
+  const product = await Product.findById(req.query.productId);
+
+  const reviews = product.reviews.filter(review => review._id.toString() !== req.query.id.toString());
+
+  const numOfReviews = reviews.length;
+
+  const ratings = product.reviews.reduce((acc, item) => item.rating + acc, 0) / reviews.length
+
+  await Product.findByIdAndUpdate(req.query.productId,{
+    reviews,
+    ratings,
+    numOfReviews
+  }, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false
+  })
+
+  res.status(200).json ({
+    succes:true; 
+  })
+})
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -19,9 +19,11 @@ router.route("/product/:id").get(getSingleProduct);
 router.route('/admin/product/new').post(isAuthenticatedUser, authorizeRoles('admin'), newProduct);
 
 router.route('/admin/product/:id')
-    .put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct)
+    .put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct);
     .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
 
-router.route('/review').put(isAuthenticatedUser, createProductReview)
+router.route('/review').put(isAuthenticatedUser, createProductReview);
+
+
 
 module.exports = router;
