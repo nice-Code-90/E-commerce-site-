9f84afd1a332ae5c96a8389833d0c1dc13239cae
PetraPongo <pongopetra99@gmail.com>
2023-12-02 02:30:24 +0100

    Termékek fetchelése (Redux állapothoz)

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,36 +1,40 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 
 import MetaData from './layout/MetaData'
+import Product from './product/product'
+
+
+import { useDispatch, useSelector } from 'react-redux'
+import { getProducts } from '../actions/productActions'
 
 const Home = () => {
+
+    const dispatch = useDispatch();
+
+    const { loading, products, error, productsCount } = useSelector(state => state.products)
+
+    useEffect(() => {
+        dispatch(getProducts());
+    }, [dispatch])
+
     return (
         <Fragment>
-            <MetaData title={'Buy Best Products Online'}></MetaData>
-            <h1 id="products_heading">Latest Products</h1>
+            {loading ? <h1>Loading...</h1> : (
+                <Fragment>
+                    <MetaData title={'Buy Best Products Online'}></MetaData>
+                    <h1 id="products_heading">Latest Products</h1>
 
-            <section id="products" className="container mt-5">
-                <div className="row">
-                    <div className="col-sm-12 col-md-6 col-lg-3">
-                        <div className="card p-3 rounded">
-                            <img className="card-img-top mx-auto"
-                                src="https://m.media-amazon.com/images/I/617NtexaW2L._AC_UY218_.jpg" />
-                            <div className="card-body d-flex flex-column">
-                                <h5 className="card-title">
-                                    <a href="">128GB Solid Storage Memory Card - SanDisk Ultra</a>
-                                </h5>
-                                <div className="ratings mt-auto">
-                                    <div className="rating-outer">
-                                        <div className="rating-inner"></div>
-                                    </div>
-                                    <span id="no_of_reviews">(5 reviews)</span>
-                                </div>
-                                <p className="card-text">$45.67</p>
-                                <a href="#" id="view_btn" className="btn btn-block">View Details</a>
-                            </div>
+                    <section id="products" className="container mt-5">
+                        <div className="row">
+                            {products && products.map(product => (
+                                <Product key={product._id} product={product}></Product>
+                            ))}
+
                         </div>
-                    </div>
-                </div>
-            </section>
+                    </section>
+                </Fragment>
+            )}
+
         </Fragment>
     )
 }
