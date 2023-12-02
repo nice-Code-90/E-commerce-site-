fa70bc979636cea8a1b5f36b7ce706bb195bed28
PetraPongo <pongopetra99@gmail.com>
2023-12-02 01:42:49 +0100

    Product Reducerek elkészítése

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -27,7 +27,6 @@ exports.getProducts = catchAsyncErrors(async (req, res, next) => {
   const products = await apiFeatures.query;
   res.status(200).json({
     success: true,
-    count: products.length,
     productCount,
     products,
   });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/layout/Footer'
 import Home from './components/Home'
 
 
+
 function App() {
   return (
     <Router>
