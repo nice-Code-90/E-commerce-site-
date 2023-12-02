4ea30d273102f9c0ec7c7a61bf947577a213dcbe
SzaboPatrik02 <145067605+SzaboPatrik02@users.noreply.github.com>
2023-12-02 20:33:20 +0100

    Frontend -- Search Products Filter by Ratings

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -19,6 +19,7 @@ const Home = ({ match }) => {
     const [currentPage, setCurrentPage] = useState(1)
     const [price, setPrice] = useState([1, 1000])
     const [category, setCategory] = useState('')
+    const [rating, setRating] = useState(0)
 
     const categories = [
         'Electronics',
@@ -49,10 +50,10 @@ const Home = ({ match }) => {
             return alert.error(error)
         }
 
-        dispatch(getProducts(keyword, currentPage, price, category));
+        dispatch(getProducts(keyword, currentPage, price, category, rating));
 
 
-    }, [dispatch, alert, error, keyword, currentPage, price, category])
+    }, [dispatch, alert, error, keyword, currentPage, price, category, rating])
 
     function setCurrentPageNo(pageNumber) {
         setCurrentPage(pageNumber)
@@ -118,6 +119,38 @@ const Home = ({ match }) => {
 
                                                 </ul>
                                             </div>
+
+                                            <hr className="my-3"/>
+
+                                            <div className="mt-5">
+                                                <h4 className="mb-3">
+                                                    Ratings
+                                                </h4>
+
+                                                <ul className="pl-0">
+                                                    {[5, 4, 3, 2, 1].map(star => (
+                                                    <li
+                                                        style={{
+                                                            cursor: 'pointer',
+                                                            listStyleType: 'none'
+                                                        }}
+                                                        key = {star}
+                                                        onClick = {() => setRating(star)
+                                                        }
+                                                    >
+                                                    <div className="rating-outer">
+                                                        <div className="rating-inner">
+                                                            style={{
+                                                                width: `${star * 20}%`
+                                                            }}
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                            ))}
+
+                                                </ul>
+                                            </div>
+
                                         </div>
                                     </div>
 
