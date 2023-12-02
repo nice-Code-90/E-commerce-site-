88c7e5c775adf152fe247c6d04bf9fe6dc31bea5
PetraPongo <pongopetra99@gmail.com>
2023-12-02 01:08:48 +0100

    Egyedi cím készítése

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 import Home from './components/Home'
+
+
 function App() {
   return (
     <Router>
