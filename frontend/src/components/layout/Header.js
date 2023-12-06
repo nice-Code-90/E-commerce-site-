37c4d47ebbd4b8843c9af0f51e012633f483fa36
zolttakacs <zoli200200@gmail.com>
2023-12-06 12:40:00 +0100

    Frontend --layout(Header.js) Link

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -4,3 +4,39 @@ import { Route, Link } from 'react-router-dom'
 import Search from './Search';
 
 import '../../App.css';
+
+const Header = () => {
+    return (
+      <Fragment>
+        <nav className="navbar row">
+          <div className="col-12 col-md-3">
+            <div className="navbar-brand">
+              <Link to="/">
+                <img src="/images/shopit_logo.png" />
+              </Link>
+              <img src="./images/logo.png" />
+            </div>
+          </div>
+  
+          <div className="col-12 col-md-6 mt-2 mt-md-0">
+            <Route render = {({history}) => <Search history={history} /> } />
+          </div>
+  
+          <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+            <Link to="/login" className="btn" id="login_btn">
+              Login
+            </Link>
+  
+            <span id="cart" className="ml-3">
+              Cart
+            </span>
+            <span className="ml-1" id="cart_count">
+              2
+            </span>
+          </div>
+        </nav>
+      </Fragment>
+    );
+  };
+  
+  export default Header;
