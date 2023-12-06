cdf6a1b95952feb811d9dbd31cc4de3a01150ca2
zolttakacs <zoli200200@gmail.com>
2023-12-06 12:43:00 +0100

    FrontEnd -- Reducers(userReducers.js) case fail,errors, success

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -5,3 +5,34 @@ import {
     CLEAR_ERRORS
   } from '../constants/userConstants'
 
+  export cons authReducer = (state ={user: {}}, action)=> {
+    swithc(action.type){
+
+        case LOGIN_REQUEST:
+            return {
+                loading: true,
+                isAuthenticated: false,
+            }
+
+        case LOGIN_SUCCES:
+            return{
+                ...state,
+                loading: false,
+                isAuthenticated: true,
+                user: action.payload
+            }
+        case LOGIN_FAIL:
+            return{
+                ...state,
+                loading: false,
+                isAuthenticated: false,
+                user: null,
+                error: action.payload
+            }
+        clear CLEAR_ERRORS:
+            return{
+                ...state,
+                error: null,
+            }
+        
+}
