5b7a8e650f6b8fbe66a9db5422893fa1f1341699
zolttakacs <zoli200200@gmail.com>
2023-12-06 12:40:46 +0100

    Frontend -- store.js const reducer, importok

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,7 +3,13 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { productsReducer, productDetailsReducer } from './reducers/productReducers';
-//
+
+import { authReducer} from './reducers/userReducers'
+
+const reducer = combineReducers({
+    products: productsReducer,
+    productDetails: productDetailsReducer
+})
 
 
 let initialState = {}
