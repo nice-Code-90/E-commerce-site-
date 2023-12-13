d92ed1ade0a88bb8dc2176297b271638c86312ab
Milan Mazik <mazik1990@hotmail.com>
2023-12-13 14:32:28 +0100

    frontend - handle quantity selection

--- frontend/src/components/product/productDetails.js
+++ frontend/src/components/product/productDetails.js
@@ -1,131 +1,214 @@
-import React, { Fragment, useEffect } from 'react'
-import { Carusel } from 'react-bootstrap'
+import React, { Fragment, useEffect, useState } from "react";
+import { Carusel } from "react-bootstrap";
 
-import Loader from '../layout/Loader'
-import MetaData from '../layout/MetaData'
+import Loader from "../layout/Loader";
+import MetaData from "../layout/MetaData";
 
-import { useAlert } from 'react-alert'
-import { useDispatch, useSelector } from 'react-redux'
-import { getProductDetails, clearErrors } from '../../actions/productActions'
+import { useAlert } from "react-alert";
+import { useDispatch, useSelector } from "react-redux";
+import { getProductDetails, clearErrors } from "../../actions/productActions";
 
 const productDetails = ({ match }) => {
+  const [quantity, setQuantity] = useState(1);
+  const dispatch = useDispatch();
+  const alert = useAlert();
 
-    const dispatch = useDispatch();
-    const alert = useAlert();
+  const { loading, error, product } = useSelector(
+    (state) => state.productDetails
+  );
 
-    const { loading, error, product } = useSelector(state => state.productDetails)
+  useEffect(() => {
+    dispatch(getProductDetails(match.params.id));
 
-    useEffect(() => {
-        dispatch(getProductDetails(match.params.id))
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+  }, [dispatch, alert, error, match.params.id]);
 
-        if (error) {
-            alert.error(error);
-            dispatch(clearErrors())
-        }
+  const increaseQty = () => {
+    const count = document.querySelector(".count");
+    if (count.valueAsNumber > +product.stock) {
+      return;
+    }
+    const qty = count.valueAsNumber + 1;
+    setQuantity(qty);
+  };
 
-    }, [dispatch, alert, error, match.params.id])
-    return (
+  const decreaseQty = () => {
+    const count = document.querySelector(".count");
+    if (count.valueAsNumber <= 1) {
+      return;
+    }
+    const qty = count.valueAsNumber - 1;
+    setQuantity(qty);
+  };
 
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
         <Fragment>
-            {loading ?
-                <Loader /> : (
-                    <Fragment>
-                        <MetaData title={product.name} />
-                        <div className="row f-flex justify-content-around">
-                            <div className="col-12 col-lg-5 im-fluid" id="product-image">
-                                <Carusel pause='hover'>
-                                    {product.images && product.images.map(image => (
-                                        <Carusel.Item key={image.public_id}>
-                                            <img className="d-block w-100" src={image.url} alt={product.title}></img>
-                                        </Carusel.Item>
-                                    ))}
-                                </Carusel>
-                            </div>
+          <MetaData title={product.name} />
+          <div className="row f-flex justify-content-around">
+            <div className="col-12 col-lg-5 im-fluid" id="product-image">
+              <Carusel pause="hover">
+                {product.images &&
+                  product.images.map((image) => (
+                    <Carusel.Item key={image.public_id}>
+                      <img
+                        className="d-block w-100"
+                        src={image.url}
+                        alt={product.title}
+                      ></img>
+                    </Carusel.Item>
+                  ))}
+              </Carusel>
+            </div>
 
-                            <div className="col-12 col-lg-5 mt-5">
-                                <h3>{product.name}</h3>
-                                <p id="product.id">Product {product._id}</p>
+            <div className="col-12 col-lg-5 mt-5">
+              <h3>{product.name}</h3>
+              <p id="product.id">Product {product._id}</p>
 
-                                <hr />
+              <hr />
 
-                                <div className="rating-outer">
-                                    <div className="rating-inner" style={({ width: `${(product.ratings / 5) * 100}%` })}></div>
-                                </div>
-                                <span id="no_of_reviews">({product.numOfReviews} Reviews)</span>
+              <div className="rating-outer">
+                <div
+                  className="rating-inner"
+                  style={{ width: `${(product.ratings / 5) * 100}%` }}
+                ></div>
+              </div>
+              <span id="no_of_reviews">({product.numOfReviews} Reviews)</span>
 
+              <hr />
 
-                                <hr />
+              <p id="product_price">${product.price}</p>
+              <div className="stockCounter d-inline">
+                <span className="btn btn-danger minus" onClick={decreaseQty}>
+                  -
+                </span>
 
-                                <p id="product_price">${product.price}</p>
-                                <div className="stockCounter d-inline">
-                                    <span className="btn btn-danger minus">-</span>
+                <input
+                  type="number"
+                  className="form-control count d-inline"
+                  value={quantity}
+                  readOnly
+                />
 
-                                    <input type="number" className="form-control count d-inline"
-                                        value="1" readOnly />
+                <span className="btn btn-primary plus" onClick={increaseQty}>
+                  +
+                </span>
+              </div>
+              <button
+                type="button"
+                id="cart_btn"
+                className="btn btn-primary d-inline ml-4"
+              >
+                Add to cart
+              </button>
 
-                                    <span className="btn btn-primary plus">+</span>
-                                </div>
-                                <button type="button" id="cart_btn" className="btn btn-primary d-inline ml-4">Add to cart</button>
+              <hr />
 
-                                <hr />
+              <p>
+                Status:{" "}
+                <span
+                  id="stock_status"
+                  className={product.stock > 0 ? "greenColor" : "redColor"}
+                >
+                  {product.stock > 0 ? "In Stock" : "Out of Stock"}
+                </span>
+              </p>
 
-                                <p>Status: <span id="stock_status"
-                                    className={product.stock > 0 ? 'greenColor' : 'redColor'}>
-                                    {product.stock > 0 ? 'In Stock' : 'Out of Stock'}</span></p>
+              <hr />
 
-                                <hr />
+              <h4 className="mt-2">Description</h4>
+              <p>{product.description}</p>
 
-                                <h4 className="mt-2">Description</h4>
-                                <p>{product.description}</p>
+              <hr />
 
-                                <hr />
+              <p id="product_seller mb-3">
+                Sold by: <strong>{product.seller}</strong>
+              </p>
 
-                                <p id="product_seller mb-3">Sold by: <strong>{product.seller}</strong></p>
-
-                                <button id="review_btn" type="button" className="btn btn-primary mt-4" data-toggle="modal" data-target="#ratingModel">
-                                    Submit your review
-                                </button>
-                                <div className="row mt-2 mb-5">
-                                    <div className="rating w-50">
-                                        <div className="modal fade" id="ratingModal" tabIndex="-1" role="dialog" aria-labelledby="ratingModalLabel" aria-hidden="true">
-                                            <div className="modal-dialog" role="document">
-                                                <div className="modal-content">
-                                                    <div className="modal-header">
-                                                        <h5 className="modal-title" id="ratingModalLabel">Submit Review</h5>
-                                                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                                            <span aria-hidden="true">&times;</span>
-                                                        </button>
-                                                    </div>
-                                                    <div className="modal-body">
-
-                                                        <ul className="stars" >
-                                                            <li className="star"><i className="fa fa-star"></i></li>
-                                                            <li className="star"><i className="fa fa-star"></i></li>
-                                                            <li className="star"><i className="fa fa-star"></i></li>
-                                                            <li className="star"><i className="fa fa-star"></i></li>
-                                                            <li className="star"><i className="fa fa-star"></i></li>
-                                                        </ul>
-
-                                                        <textarea name="review" id="review" className="form-control mt-3">
-
-                                                        </textarea>
-
-                                                        <button className="btn my-3 float-right review-btn px-4 text-white" data-dismiss="modal" aria-label="Close">Submit</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-
-                            </div>
+              <button
+                id="review_btn"
+                type="button"
+                className="btn btn-primary mt-4"
+                data-toggle="modal"
+                data-target="#ratingModel"
+              >
+                Submit your review
+              </button>
+              <div className="row mt-2 mb-5">
+                <div className="rating w-50">
+                  <div
+                    className="modal fade"
+                    id="ratingModal"
+                    tabIndex="-1"
+                    role="dialog"
+                    aria-labelledby="ratingModalLabel"
+                    aria-hidden="true"
+                  >
+                    <div className="modal-dialog" role="document">
+                      <div className="modal-content">
+                        <div className="modal-header">
+                          <h5 className="modal-title" id="ratingModalLabel">
+                            Submit Review
+                          </h5>
+                          <button
+                            type="button"
+                            className="close"
+                            data-dismiss="modal"
+                            aria-label="Close"
+                          >
+                            <span aria-hidden="true">&times;</span>
+                          </button>
                         </div>
-                    </Fragment>
-                )
-            }
-        </Fragment >
-    )
-}
+                        <div className="modal-body">
+                          <ul className="stars">
+                            <li className="star">
+                              <i className="fa fa-star"></i>
+                            </li>
+                            <li className="star">
+                              <i className="fa fa-star"></i>
+                            </li>
+                            <li className="star">
+                              <i className="fa fa-star"></i>
+                            </li>
+                            <li className="star">
+                              <i className="fa fa-star"></i>
+                            </li>
+                            <li className="star">
+                              <i className="fa fa-star"></i>
+                            </li>
+                          </ul>
 
-export default productDetails
+                          <textarea
+                            name="review"
+                            id="review"
+                            className="form-control mt-3"
+                          ></textarea>
+
+                          <button
+                            className="btn my-3 float-right review-btn px-4 text-white"
+                            data-dismiss="modal"
+                            aria-label="Close"
+                          >
+                            Submit
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+export default productDetails;
