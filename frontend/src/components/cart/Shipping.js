a23b818b817ee7635049d83458e81ae7441f3c99
PetraPongo <pongopetra99@gmail.com>
2023-12-13 21:54:40 +0100

    Checkout - Confrirm order

--- frontend/src/components/cart/Shipping.js
+++ frontend/src/components/cart/Shipping.js
@@ -2,6 +2,8 @@ import React, { Fragment, useState } from 'react'
 import { countries } from 'countries-list';
 
 import MetaData from "../layout/MetaData";
+import CheckoutSteps from './CheckoutSteps';
+
 import { useDispatch, useSelector } from "react-redux";
 import { saveShippingInfo } from '../../actions/cartActions'
 
@@ -36,7 +38,9 @@ const Shipping = ({ history }) => {
 
     return (
         <Fragment>
-            <MetaData title={'Shipping Info'}></MetaData>
+            <MetaData title={'Shipping Info'} />
+            <CheckoutSteps shipping />
+
             <div className="row wrapper">
                 <div className="col-10 col-lg-5">
                     <form className="shadow-lg" onSubmit={submitHandler}>
